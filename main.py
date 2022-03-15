7d3528bdf5391eeb9a92fe1a8b6cf4330196deb7
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-03-15 16:01:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ PyrogramBot = Client(
     "PyrogramBot",
     api_hash="d9647913e97bf2f6a66d978290284028",
     api_id="17946666",
-    bot_token="5125014420:AAEK2E9aj-q9FHZknW_hrisJvCuONx0XPtU"
+    bot_token="5125014420:AAEK2E9aj-q9FHZknW_hrisJvCuONx0XPtU",
     plugins=dict(root="PyrogramBot")
 )
 
