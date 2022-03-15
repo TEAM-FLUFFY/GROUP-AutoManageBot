4a618466a8ff71de5d1781c6d3595b3b9e1166a6
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-03-15 15:57:17 +0530

    Delete Plugins directory

--- Plugins/callback.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_callback_query()
-async def cb(bot, msg):
-    if msg.data == "start":
-     await msg.message.edit("hi")
--- Plugins/commands.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("start"))
-async def start_cmd(bot, msg):
-    await msg.reply_text("hey bro uam")
