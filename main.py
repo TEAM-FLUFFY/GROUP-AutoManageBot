6ab7dcb6d54c0d368d2c5e4fbbbc8c2485944e2f
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-03-15 09:12:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+
+
+dev = Client(
+    "Bot",
+    api_hash="d9647913e97bf2f6a66d978290284028",
+    api_id="17946666",
+    bot_token="5125014420:AAEK2E9aj-q9FHZknW_hrisJvCuONx0XPtU"
+    plugins=dict(root="Plugins")
+)
+
+
+
+dev.run()
