5e78ad5f6eaba5d53178ee83a53ce3f337156490
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-03-16 16:22:45 +0530

    Update callback.py

--- PyrogramBot/callback.py
+++ PyrogramBot/callback.py
@@ -1,4 +1,7 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import CallbackQuery
+import random
 
 @Client.on_callback_query()
 async def cb(bot, msg):
