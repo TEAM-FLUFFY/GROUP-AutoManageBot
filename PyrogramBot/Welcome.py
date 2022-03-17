0c18600725c50a65556ffcc247c5930a01d7d131
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-03-17 21:15:40 +0530

    Create Welcome.py

---
+++ PyrogramBot/Welcome.py
@@ -0,0 +1,22 @@
+import os
+import pyrogram
+from pyrogram import filters, Client
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+import logging
+
+
+
+
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+ chatid= message.chat.id
+ await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+ 
+@Client.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+ chatid= message.chat.id
+ await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
