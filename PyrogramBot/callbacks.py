cc6ef843d266768cbf7a8df3030af588d81a85bd
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-03-18 20:09:33 +0530

    Update callbacks.py

--- PyrogramBot/callbacks.py
+++ PyrogramBot/callbacks.py
@@ -6,9 +6,9 @@ from pyrogram.types import CallbackQuery
 
 @Client.on_callback_query()
 async def telegraph_cb(bot, msg)
-    if msg.data == "start"
+    if msg.data == "tgra"
         await.msg.message.edit(
-            text="""▫️HELP: Telegraph▪️
+            text="""<b>HELP:</b> Telegraph▪️
 
 Do as you wish with telegra.ph module!
 
@@ -25,3 +25,23 @@ NOTE:
                ]]
                )
         )
+
+
+    elif msg.data == "start"
+        await msg.message.edit(
+            text=START_MESSAGE.format(message.from_user.mention),
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton ("🗨️𝔾ℝ𝕆𝕌ℙ🗨️", url="https://t.me/DEVELOPERSCHANNEL2022"),
+                InlineKeyboardButton ("📂ℂℍ𝔸ℕℕ𝔼𝕃📂", url="https://t.me/DELCHANNEL001")
+                ],[
+                InlineKeyboardButton ("🔰𝔼𝔻𝕀𝕋𝔼ℝ🔰", url="t.me/TEAM_KERALA"),
+                InlineKeyboardButton ("©️ℙ𝔸𝕀𝔻 ℙℝ𝕆𝕄𝕆𝕋𝕀𝕆ℕ", url="t.me/pushpa_Reju")
+                ],[
+                InlineKeyboardButton ("👨‍💻𝔻𝔼𝕍𝔼𝕃𝕆ℙ𝔼ℝ👨‍💻", url="t.me/TEAM_KERALA"),
+                InlineKeyboardButton ("help", callback_data="help")
+                ],[
+                InlineKeyboardButton ("⚜️𝔸𝔻𝔻 𝕄𝔼 𝕋𝕆 𝔸 ℂℍ𝔸𝕋 𝔾ℝ𝕆𝕌ℙ⚜️", url="http://t.me/FluffyPyroGramBot?startgroup=true")
+                ]]
+                )
+        )
+    
